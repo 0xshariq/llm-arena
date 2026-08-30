3693f932814de4e6af33b16a55a4ba260ec8016e
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-08-30 05:59:28 +0000

    install packages

--- module.mjs
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { module } from "@prisma/composer";
-import llmArenaService from "./service.mjs";
-
-export default module("llm-arena", ({ provision }) => {
-  provision(llmArenaService, { id: "llmarena" });
-});
